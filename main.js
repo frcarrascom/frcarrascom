bb1746fd98d7a141777316a836ad5f7442b097cf
frcarrascom <carrascomerchan@msn.com>
2020-06-09 18:57:45 +0200

    almost finished

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
 $(document).ready(function () {
 
 });
+
+
+/*    Nav scroll down    */
+
+var mywindow = $(window);
+var mypos = mywindow.scrollTop();
+var up = false;
+var newscroll;
+mywindow.scroll(function () {
+    newscroll = mywindow.scrollTop();
+    if (newscroll > mypos && !up) {
+        $('.top-nav').stop().fadeOut();
+        up = !up;
+        console.log(up);
+    } else if(newscroll < mypos && up) {
+        $('.top-nav').stop().fadeIn();
+        up = !up;
+    }
+    mypos = newscroll;
+});
+
+/*    Nav scroll down end   */
