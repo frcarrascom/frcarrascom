c068e299e64cc1881727864c0e2ea68e46a1224a
frcarrascom <carrascomerchan@msn.com>
2020-06-12 14:16:31 +0200

    cambios header

--- main.js
+++ main.js
@@ -25,5 +25,32 @@ mywindow.scroll(function () {
 /*    Nav scroll down end   */
 
 
+function myFunction() {
+    var x = document.getElementById("mylinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+
+  $(function() {
+    $(".dropdown-menu > li > a.trigger").on("click", function(e) {
+      var current = $(this).next();
+      var grandparent = $(this).parent().parent();
+      if ($(this).hasClass('left-caret') || $(this).hasClass('right-caret')) $(this).toggleClass('right-caret left-caret');
+      grandparent.find('.left-caret').not(this).toggleClass('right-caret left-caret');
+      grandparent.find(".sub-menu:visible").not(current).hide();
+      current.toggle();
+      e.stopPropagation();
+    });
+    $(".dropdown-menu > li > a:not(.trigger)").on("click", function() {
+      var root = $(this).closest('.dropdown');
+      root.find('.left-caret').toggleClass('right-caret left-caret');
+      root.find('.sub-menu:visible').hide();
+    });
+  });
+
 
 
